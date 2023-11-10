8c5968013cb64eed8da455e7261f99824e5c5150
Valentín Peñalver <valenpga@gmail.com>
2023-11-10 17:48:54 +0100

    añadir elementos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
  // Aquí tu código
+/*const body = document.querySelectorAll("#body")*/
+const elementUl = document.getElementById("lista");
+const elementButton = document.getElementById("agregar");
+
+elementButton.addEventListener("click", function(){
+    const element = prompt("Escribe tu elemento");
+    elementUl.insertAdjacentHTML("afterbegin",`<li>${element}</li>`)
+    
+
+});
+
+
+
+
+
+
+
